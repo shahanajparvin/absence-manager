f791bbb66c9d5694042ff6cab73f19fcf37808fc
shahanajparvin <putulcse9@gmail.com>
2024-12-12 19:46:04 +0600

    feat(localization): Implement internationalization support using intl package
    - Added intl package for localization
    - Created translation files for multi-language support
    - Configured localization delegate and supported locales
    - Implemented basic localization infrastructure
    - Added initial translation keys for core application strings

---
+++ lib/core/utils/core_utils.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+
+extension LocalizationExtension on BuildContext {
+  AppLocalizations get text => AppLocalizations.of(this)!;
+}
+
+
+
+
+
