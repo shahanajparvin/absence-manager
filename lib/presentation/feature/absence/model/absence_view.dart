1a7e2b80e751ab57c1d743f8fa6ecdfc79581a6e
shahanajparvin <putulcse9@gmail.com>
2024-12-14 12:25:24 +0600

    Add: Add AbsenceView class to represent absence list with employee information in the presentation layer

---
+++ lib/presentation/feature/absence/model/absence_view.dart
@@ -0,0 +1,17 @@
+class AbsenceView{
+  final String employeeName;
+  final String employeeProfile;
+  final String type;
+  final String startDate;
+  final String endDate;
+  final String status;
+
+  AbsenceView({
+    required this.employeeName,
+    required this.employeeProfile,
+    required this.type,
+    required this.startDate,
+    required this.endDate,
+    required this.status,
+  });
+}
