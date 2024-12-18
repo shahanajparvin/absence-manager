db02347e401bc8825cbfe21f8fee26ad62a2d46f
shahanajparvin <putulcse9@gmail.com>
2024-12-18 12:40:01 +0600

    Add: Add utility classes for responsive text sizes, dimensions, and rounding using flutter_screenutil"

---
+++ lib/core/utils/app_size.dart
@@ -0,0 +1,121 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppTextSize {
+  static double s0 = 0.0.sp;
+  static double s2 = 2.0.sp;
+  static double s3 = 3.0.sp;
+  static double s4 = 4.0.sp;
+  static double s5 = 5.0.sp;
+  static double s6 = 6.0.sp;
+  static double s8 = 8.0.sp;
+  static double s10 = 10.0.sp;
+  static double s11 = 11.0.sp;
+  static double s12 = 12.0.sp;
+  static double s13 = 13.0.sp;
+  static double s14 = 14.0.sp;
+  static double s15 = 15.0.sp;
+  static double s16 = 16.0.sp;
+  static double s17 = 17.0.sp;
+  static double s18 = 18.0.sp;
+  static double s20 = 20.0.sp;
+  static double s22 = 22.0.sp;
+  static double s24 = 24.0.sp;
+  static double s25 = 25.0.sp;
+}
+
+class AppHeight {
+  static double s0 = 0.0.h;
+  static double s1 = 0.0.h;
+  static double s2 = 2.0.h;
+  static double s3 = 3.0.h;
+  static double s4 = 4.0.h;
+  static double s5 = 5.0.h;
+  static double s6 = 6.0.h;
+  static double s8 = 8.0.h;
+  static double s10 = 10.0.h;
+  static double s11 = 11.0.h;
+  static double s12 = 12.0.h;
+  static double s13 = 13.0.h;
+  static double s14 = 14.0.h;
+  static double s15 = 15.0.h;
+  static double s16 = 16.0.h;
+  static double s17 = 17.0.h;
+  static double s18 = 18.0.h;
+  static double s19 = 19.0.h;
+  static double s20 = 20.0.h;
+  static double s22 = 22.0.h;
+  static double s24 = 24.0.h;
+  static double s25 = 25.0.h;
+  static double s26 = 26.0.h;
+  static double s28 = 28.0.h;
+  static double s30 = 30.0.h;
+  static double s32 = 32.0.h;
+  static double s34 = 34.0.h;
+  static double s35 = 35.0.h;
+  static double s36 = 36.0.h;
+  static double s38 = 38.0.h;
+  static double s40 = 40.0.h;
+  static double s42 = 42.0.h;
+  static double s44 = 44.0.h;
+  static double s45 = 45.0.h;
+  static double s48 = 48.0.h;
+  static double s50 = 50.0.h;
+
+}
+
+class AppWidth {
+  static double s0 = 0.0.w;
+  static double s1 = 1.0.w;
+  static double s2 = 2.0.w;
+  static double s3 = 3.0.w;
+  static double s4 = 4.0.w;
+  static double s5 = 5.0.w;
+  static double s6 = 6.0.w;
+  static double s8 = 8.0.w;
+  static double s9 = 9.0.w;
+  static double s10 = 10.0.w;
+  static double s11 = 11.0.w;
+  static double s12 = 12.0.w;
+  static double s13 = 13.0.w;
+  static double s14 = 14.0.w;
+  static double s15 = 15.0.w;
+  static double s16 = 16.0.w;
+  static double s17 = 17.0.w;
+  static double s18 = 18.0.w;
+  static double s20 = 20.0.w;
+  static double s22 = 22.0.w;
+  static double s24 = 24.0.w;
+  static double s25 = 25.0.w;
+  static double s28 = 28.0.w;
+  static double s30 = 30.0.w;
+  static double s32 = 32.0.w;
+  static double s34 = 34.0.w;
+  static double s35 = 35.0.w;
+  static double s36 = 36.0.w;
+  static double s38 = 38.0.w;
+  static double s40 = 40.0.w;
+  static double s48 = 48.0.w;
+  static double s50 = 50.0.w;
+
+}
+
+class AppRound {
+  static double s0 = 0.0.r;
+  static double s2 = 2.0.r;
+  static double s3 = 3.0.r;
+  static double s4 = 4.0.r;
+  static double s5 = 5.0.r;
+  static double s6 = 6.0.r;
+  static double s8 = 8.0.r;
+  static double s10 = 10.0.r;
+  static double s11 = 11.0.r;
+  static double s12 = 12.0.r;
+  static double s13 = 13.0.r;
+  static double s14 = 14.0.r;
+  static double s15 = 15.0.r;
+  static double s16 = 16.0.r;
+  static double s17 = 17.0.r;
+  static double s18 = 18.0.r;
+  static double s20 = 20.0.r;
+
+}
