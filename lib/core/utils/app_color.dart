b891ca4350c0906aa297095489eb67f099697947
shahanajparvin <putulcse9@gmail.com>
2024-12-18 11:01:08 +0600

    Add: Add theme and utility colors to AppColor class

---
+++ lib/core/utils/app_color.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+
+
+  static const Color appBarColor = Color(0xff005F6A);
+
+  static const Color themeColor =  Color(0xff005F6A);
+
+  static final Color splashColor = themeColor.withOpacity(0.5);
+
+  static const Color activeColor = Color(0xffE97D32);
+
+  static const Color whiteColor = Color(0xffFFFFFF);
+
+  static const Color greyColor = Color(0xffD5D8DE);
+
+  static const Color lightGreyColor = Color(0xffEDEDED);
+
+  static const Color darkGreyColor = Color(0xffD0D0D0);
+
+
+}
