f594a69bfb2a1404baba99c276809de926618d9c
shahanajparvin <putulcse9@gmail.com>
2024-12-19 18:53:46 +0600

    chore: add AppImage class for managing SVG icon assets

---
+++ lib/core/utils/app_image.dart
@@ -0,0 +1,10 @@
+
+import 'dart:core';
+
+class AppImage {
+  AppImage._();
+  static const String icCalender = 'assets/icons/ic_calender.svg';
+
+  static const String icFilterHeader = 'assets/icons/ic_filter_header.svg';
+
+}
