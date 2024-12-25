e1b684a49e64428a74283f0b0abae423919b433d
shahanajparvin <putulcse9@gmail.com>
2024-12-25 14:02:19 +0600

    Add: Add new SVG icons for absence details page

--- lib/core/utils/app_image.dart
+++ lib/core/utils/app_image.dart
@@ -6,5 +6,12 @@ class AppImage {
   static const String icCalender = 'assets/icons/ic_calender.svg';
   static const String icFilterHeader = 'assets/icons/ic_filter_header.svg';
   static const String icFilter = 'assets/icons/ic_filter.svg';
-
+  static const String icName = 'assets/icons/ic_name.svg';
+  static const String icAbsenceType = 'assets/icons/ic_absence_type.svg';
+  static const String icStartDate = 'assets/icons/ic_start_date.svg';
+  static const String icEndDate = 'assets/icons/ic_end_date.svg';
+  static const String icStatus = 'assets/icons/ic_status.svg';
+  static const String icTotalCount = 'assets/icons/ic_date_count.svg';
+  static const String icMemberNote = 'assets/icons/ic_memeber_note.svg';
+  static const String icAdmitTerNote = 'assets/icons/ic_admitter_note.svg';
 }
