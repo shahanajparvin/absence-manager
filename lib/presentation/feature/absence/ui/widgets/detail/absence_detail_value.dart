5f3af3658638c6e600c91f50259494681aedac22
shahanajparvin <putulcse9@gmail.com>
2024-12-25 14:00:11 +0600

    Add:Add AbsenceDetailValue widget for displaying absence detail values

---
+++ lib/presentation/feature/absence/ui/widgets/detail/absence_detail_value.dart
@@ -0,0 +1,19 @@
+import 'package:absence_manager/core/utils/app_color.dart';
+import 'package:flutter/material.dart';
+
+
+class AbsenceDetailValue extends StatelessWidget {
+  final String value;
+  final Color? color;
+
+  const AbsenceDetailValue({super.key, required this.value, this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      value,
+      style: Theme.of(context).textTheme.labelMedium!.copyWith(
+          fontWeight: FontWeight.w500, color: color ?? AppColor.textColorDark),
+    );
+  }
+}
