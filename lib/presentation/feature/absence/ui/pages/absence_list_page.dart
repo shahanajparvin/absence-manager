825aaf84643f6d6cc6c8498e4b80b002a30b4ee0
shahanajparvin <putulcse9@gmail.com>
2024-12-29 17:13:59 +0600

    Refactor: Refactor AbsenceListPage to remove FutureBuilder and simplify app settings initialization.

--- lib/presentation/feature/absence/ui/pages/absence_list_page.dart
+++ lib/presentation/feature/absence/ui/pages/absence_list_page.dart
@@ -4,6 +4,7 @@ import 'package:absence_manager/core/utils/app_modal_controller.dart';
 import 'package:absence_manager/core/utils/app_settings.dart';
 import 'package:absence_manager/core/utils/app_size.dart';
 import 'package:absence_manager/core/utils/core_utils.dart';
+import 'package:absence_manager/main_common.dart';
 import 'package:absence_manager/presentation/feature/absence/bloc/absence_list/absence_list_bloc.dart';
 import 'package:absence_manager/presentation/feature/absence/ui/widgets/list/absence_list.dart';
 import 'package:absence_manager/presentation/feature/common/widgets/app_translate_icon.dart';
@@ -21,52 +22,35 @@ class AbsenceListPage extends StatefulWidget {
 class _AbsenceListPageState extends State<AbsenceListPage> {
   final ModalController modalController = injector();
 
-  Future<AppSettings> _initializeAppSettings() async {
-    return await AppSettings.create();
-  }
+  final ChangeLanguageView changeLanguageView = ChangeLanguageView(
+    appSettings: appSettings,
+  );
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<AppSettings>(
-      future: _initializeAppSettings(),
-      builder: (BuildContext context, AsyncSnapshot<AppSettings> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return  Center(child: Text(context.text.general_error_message));
-        } else if (snapshot.hasData) {
-          final AppSettings appSettings = snapshot.data!;
-          final ChangeLanguageView changeLanguageView = ChangeLanguageView(
+    return Scaffold(
+      backgroundColor: AppColor.whiteColor,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Padding(
+            padding: EdgeInsets.only(left: AppWidth.s10),
+            child: Text(
+              context.text.absence_manager,
+              style: context.textTheme.displayMedium,
+            )),
+        actions: <Widget>[
+          TranslateIcon(
+            modalController: modalController,
             appSettings: appSettings,
-          );
-          return Scaffold(
-            backgroundColor: AppColor.whiteColor,
-            appBar: AppBar(
-              backgroundColor: Colors.white,
-              title: Padding(
-                  padding: EdgeInsets.only(left: AppWidth.s10),
-                  child: Text(
-                    context.text.absence_manager,
-                    style: context.textTheme.displayMedium,
-                  )),
-              actions: <Widget>[
-                TranslateIcon(
-                  modalController: modalController,
-                  appSettings: appSettings,
-                  changeLanguageView: changeLanguageView,
-                  modalContext: context,
-                ),
-              ],
-            ),
-            body: BlocProvider<AbsenceListBloc>(
-              create: (_) => injector(),
-              child: AbsenceListWidget(),
-            ),
-          );
-        } else {
-          return const SizedBox();
-        }
-      },
+            changeLanguageView: changeLanguageView,
+            modalContext: context,
+          ),
+        ],
+      ),
+      body: BlocProvider<AbsenceListBloc>(
+        create: (_) => injector(),
+        child: AbsenceListWidget(),
+      ),
     );
   }
 }
